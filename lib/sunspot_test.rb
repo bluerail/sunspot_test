c38ee6f7d91100ede0e1c26e5215b4c15a37888f
Martijn Storck <martijn@lico.nl>
2017-03-15 10:52:06 +0100

    Hard code ping URI to tenant 1 on port 8981

--- lib/sunspot_test.rb
+++ lib/sunspot_test.rb
@@ -67,7 +67,7 @@ module SunspotTest
 
     def solr_running?
       begin
-        solr_ping_uri = URI.parse("#{Sunspot.session.config.solr.url}/admin/ping")
+        solr_ping_uri = URI.parse("http://127.0.0.1:8981/solr/1/admin/ping")
         res = Net::HTTP.get_response(solr_ping_uri)
         # Solr will return 503 codes when it's starting up
         res.code != '503'
